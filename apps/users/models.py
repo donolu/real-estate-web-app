43ed6239a3fa63107d51b3ff437f8e611a696cae
Olumide Ibilaiye <olumideibilaiye@gmail.com>
2025-01-18 00:33:07 +0000

    feat: Add custom user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,42 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    pkid = models.AutoField(primary_key=True, editable=False)
+    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(verbose_name=_("Username"), max_length=50, unique=True)
+    first_name = models.CharField(verbose_name=_("First Name"), max_length=50)
+    last_name = models.CharField(verbose_name=_("Last Name"), max_length=50)
+    email = models.EmailField(verbose_name=_("Email Address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
+
+    def get_short_name(self):
+        return self.first_name.title()
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
